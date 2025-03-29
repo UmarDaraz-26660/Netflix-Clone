ec73be0edc7a7d7921f90c68e83a0ff54b89bc47
UmarDaraz-26660 <134763608+UmarDaraz-26660@users.noreply.github.com>
2025-03-29 13:13:15 +0500

    update index.html

---
+++ script.js
@@ -0,0 +1,56 @@
+alert("Netflix | Pakistan By Dev. Umar Daraz" )
+// for ans1
+function showanswer1() {
+    const ans1= document.querySelector('.ans1')
+    ans1.style.display = 'flex'
+}
+function noshow1() {
+    const ans1= document.querySelector('.ans1')
+    ans1.style.display = 'none'
+}
+// for ans2
+
+function showanswer2() {
+    const ans2= document.querySelector('.ans2')
+    ans2.style.display = 'flex'
+}
+function noshow2() {
+    const ans2= document.querySelector('.ans2')
+    ans2.style.display = 'none'
+}
+// for ans3
+function showanswer3() {
+    const ans3= document.querySelector('.ans3')
+    ans3.style.display = 'flex'
+}
+function noshow3() {
+    const ans3= document.querySelector('.ans3')
+    ans3.style.display = 'none'
+}
+// for ans4
+function showanswer4() {
+    const ans4= document.querySelector('.ans4')
+    ans4.style.display = 'flex'
+}
+function noshow4() {
+    const ans4= document.querySelector('.ans4')
+    ans4.style.display = 'none'
+}
+// for ans5
+function showanswer5() {
+    const ans5= document.querySelector('.ans5')
+    ans5.style.display = 'flex'
+}
+function noshow5() {
+    const ans5= document.querySelector('.ans5')
+    ans5.style.display = 'none'
+}
+// for ans6
+function showanswer6() {
+    const ans6= document.querySelector('.ans6')
+    ans6.style.display = 'flex'
+}
+function noshow6() {
+    const ans6= document.querySelector('.ans6')
+    ans6.style.display = 'none'
+}
